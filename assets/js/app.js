57905ee7ac147aa90fc56adbb340ad2adb6a7a54
BereniceSan <sanchezberenice09@gmail.com>
2023-01-18 10:41:32 -0600

    Revisión código - Ejercicio 01

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,32 @@
+
+/*
+Breve descripcion
+El codigo muestra retorna datos de la API 'https://api.github.com' 
+haciendo uso de una funcion async/await (proceso secuencial)
+
+*/
+
 const baseEndpoint = 'https://api.github.com';
 const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
-const $b = document.querySelector('#blog');
+const $n = document.querySelector('.name');
+const $b = document.querySelector('.blog');
 const $l = document.querySelector('.location');
 
-function displayUser(username) {
+//await funciona junto con una función async
+async function displayUser(username) {
   $n.textContent = 'cargando...';
   const response = await fetch(`${usersEndpoint}/${username}`);
   console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
+  //Se referenció a la variable $n correctamente
+  $n.textContent = `${data.name}`;
+  $b.textContent = `${data.blog}`;
+  $l.textContent = `${data.location}`;
 }
 
 function handleError(err) {
   console.log('OH NO!');
   console.log(err);
-  n.textContent = `Algo salió mal: ${err}`
+  $n.textContent = `Algo salió mal: ${err}`
 }
 
-displayUser('stolinski').catch(handleError);
+displayUser('stolinski').catch(handleError());
